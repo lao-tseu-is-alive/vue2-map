f9a4729cd6c29c836b8d63f59af3bbc77294c35a
lao.tseu.is.alive@gmail.com <lao.tseu.is.alive@gmail.com>
2017-10-16 17:57:19 +0200

    added select to choose  base layer

--- src/components/vue2MapOlSwiss21781.vue
+++ src/components/vue2MapOlSwiss21781.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="vue2-map">
     <slot></slot>
+
+    <div id="oltoolbar" class="ol-toolbar">
+      <button class="map-control">Hello</button>
+      <select id="layerSelector" class="ol-layer-selector map-control"
+              v-on:change="changeLayer" v-model="activeLayer"
+      title="Cliquez pour sélectionner le fond de plan">
+        <option value="fonds_geo_osm_bdcad_couleur">Plan ville couleur</option>
+        <option value="fonds_geo_osm_bdcad_gris">Plan cadastral (gris)</option>
+        <option value="orthophotos_ortho_lidar_2016">Orthophotos 2016</option>
+        <option value="orthophotos_ortho_lidar_2012">Orthophotos 2012</option>
+      </select>
+    </div>
+    <div id="mousepos"></div>
+
   </div>
 </template>
 
@@ -29,7 +43,8 @@
         msg: 'Basic OpenLayers Map',
         ol_map: null,
         ol_view: null,
-        ol_mousePosition: null
+        ol_mousePosition: null,
+        activeLayer: 'fonds_geo_osm_bdcad_couleur'
       }
     },
     props: {
@@ -46,21 +61,37 @@
         default: 'opentopomap'
       }
     },
+    methods: {
+      changeLayer: function (event) {
+        let selectedLayer = event.target.value
+        let layers = this.ol_map.getLayers()
+        layers.forEach((layer) => {
+          console.log(`## in changeLayer layers.forEach: layer = ${layer.get('title')}`, layer)
+          let layerName = layer.get('source').layer_
+          if (layer.get('type') === 'base') {
+            if (layerName === selectedLayer) {
+              layer.setVisible(true)
+            } else {
+              layer.setVisible(false)
+            }
+          }
+        })
+      }
+    },
     mounted () {
       Proj.addProjection(swissProjection)
       // https://golux.lausanne.ch/goeland/objet/pointfixe.php?idobjet=111351
       const coordPfa180Stfrancois = [538224.21, 152378.17] // PFA3 180 - St-Francois
-      console.log(`## vue2MapOlLonLat mounted with baselayer :${this.baseLayer}`)
       console.log(`PFA3 180 - St-Francois en 21781 : ${coordPfa180Stfrancois[0]}, ${coordPfa180Stfrancois[1]}`)
       const pfa180In4326 = Conv21781To4326(coordPfa180Stfrancois[0], coordPfa180Stfrancois[1])
       console.log(`PFA3 180 - St-Francois en 4326  : ${pfa180In4326.x}, ${pfa180In4326.y} `)
       this.ol_mousePosition = new MousePosition({
-        coordinateFormat: OlCoordinate.createStringXY(2),
+        coordinateFormat: OlCoordinate.createStringXY(1),
         projection: 'EPSG:2181',
         // comment the following two lines to have the mouse position
         // be placed within the map.
-        // className: 'custom-mouse-position',
-        // target: document.getElementById('mouse-position'),
+        className: 'map-mouse-position',
+        target: document.getElementById('mousepos'),
         undefinedHTML: '&nbsp;'
       })
       this.ol_view = new View({
@@ -103,13 +134,62 @@
 
   @import "../styles/ol.css";
 
+  $toolbar_height: 1.8em;
+  $button_size: 1.5em;
+
   .vue2-map {
     width: 100%;
     height: 100%;
     margin: 0px;
     padding: 0px;
   }
-  .ol-mouse-position {
+
+  .map-mouse-position {
     background-color: white;
+    position: fixed;
+    bottom: 0em;
+    left: 0.1em;
+    height: 1.1em;
+    width: auto;
+    line-height: 1em;
+    z-index: 300;
+  }
+
+  .ol-zoom {
+    top: $toolbar_height+1.5;
+    .ol-zoom-in {
+      width: $button_size;
+      height: $button_size;
+    }
+    .ol-zoom-out {
+      width: $button_size;
+      height: $button_size;
+    }
+  }
+
+  .ol-toolbar {
+    background-color: rgba(0,60,136,0.5);
+    font-size: 1.2em;
+    position: absolute;
+    z-index: 250;
+    top: 0px;
+    left: 0px;
+    height: $toolbar_height;
+    width: 100%;
+    padding-top: 0.2em;
+    border-radius: 4px;
+    .map-control {
+      font-size: $toolbar_height - 0.8;
+      height: $toolbar_height - 0.4;
+      background-color: white;
+      border-radius: 4px;
+    }
+    button {
+      height: $button_size;
+    }
+    .ol-layer-selector {
+      right: 0.2em;
+      position: absolute;
+    }
   }
 </style>
